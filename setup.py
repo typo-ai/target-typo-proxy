b8381ccf5d8da5b611c2217185bd6fb560789581
Typo Team <59179050+typo-team@users.noreply.github.com>
2020-03-17 13:38:50 -0300

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def read(*paths):
 
 setup(
     name='target-typo-proxy',
-    version='0.1.0',
+    version='0.2.0',
     description=(
         'Typo is the intelligent data quality barrier for '
         'enterprise information systems. The Typo target proxy '
